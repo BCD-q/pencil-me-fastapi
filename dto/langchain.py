9543f4ad2d73a302442036a6a25fc24a2db534f8
Wonjeong Kim <codingshark@inu.ac.kr>
2024-03-27 09:19:20 +0900

    [Fix] LangChainResponse description 변경

--- dto/langchain.py
+++ dto/langchain.py
@@ -6,7 +6,9 @@ class LangChainResponse(BaseModel):
     memberId: int = Field(description="""Include in prefix 'member_id' in the memberId""")
     categoryId: int = Field(description="Include in prefix 'category_id' in categoryId")
     title: str = Field(description="""In the title, include a one- or two-line summary sentence 
-                                      that summarizes in the suffix 'user_dialog'""")
+                                      that summarizes in the 'user_dialog' <important!>it must be summarized and Must be related to user_dialog.</important!>
+                                      <example>"내일 저녁에 친구랑 만나서 놀기로 했어" is "친구와의 만남"</example>
+                                      """)
     contents: str = Field(description="Take notes summarizing the user's sentence. If there is no content, output null "
                                       "<important!>None is null</important> ")
     deadline: datetime = Field(description="Inside the deadline, you can infer the deadline as a date and time "
